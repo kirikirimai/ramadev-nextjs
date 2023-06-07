d17f49e3a47c6ecadc99a52aba29d7533e239cb7
kirikirimai <go.revolution.no3@gmail.com>
2023-06-07 18:37:01 +0900

    first commit

---
+++ src/app/blog/[id]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlockPost = () => {
+  return (
+    <div>BlockPost</div>
+  )
+}
+
+export default BlockPost
---
+++ src/app/csrpage/page.jsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+
+const CsrPage = () => {
+  return (
+    <div>CsrPage</div>
+  )
+}
+
+export default CsrPage
